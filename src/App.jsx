18edf9d90ea4d61706001a99e7f6df9025e8e6f4
Porson <porsonlee@gmail.com>
2022-02-18 16:20:18 -0800

    added nav bar

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+// components
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <main>
+      <div className="NavBar">
+        <NavBar />
+      </div>
+      {/* <div className="App">
+        <h2>hello world</h2>
+        <p>Welcome to my Todo App</p>
+      </div> */}
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="container">
+      <div className="nav">
+        <h2>Tasks Lists</h2>
+        <button>Add List</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
