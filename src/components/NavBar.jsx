bea9137bcd5a5fe2226abe545d621727e84d315f
Porson <porsonlee@gmail.com>
2022-02-20 20:27:11 -0800

    need to figure out how to add card

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import "./navbar.css";
 
-const NavBar = () => {
-  const [list, setList] = useState("");
+const NavBar = ({ addCard }) => {
+  const [list, setList] = useState({ id: "", title: "" });
 
   return (
     <nav className="container">
       <div className="nav">
         <h2>Tasks Lists</h2>
         <form className="add-card">
-          <input />
+          <input
+            value={list.title}
+            onChange={(e) => {
+              setList({ title: e.target.value });
+            }}
+          />
           <button>Add List</button>
         </form>
       </div>
