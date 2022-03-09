d677458ce1e48a561ba5ca4773b0c4bb615d64b1
Porson <porsonlee@gmail.com>
2022-03-09 15:57:34 -0800

    created a task useState for event handler on change when user type the input

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import "./TodoForm.css";
 
 const TodoForm = () => {
+  const [task, setTask] = useState({ id: "", todo: "", isCompleted: false });
+
   return (
     <form className="form">
-      <input />
+      <input
+        value={task.todo}
+        onChange={(e) => setTask({ todo: e.target.value })}
+      />
       <button>Add Task</button>
     </form>
   );
