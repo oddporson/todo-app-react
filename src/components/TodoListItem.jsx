a3248c950fc9f51b35fd8e07e3d76e2b5dee0c88
Porson <porsonlee@gmail.com>
2022-03-24 15:41:32 -0700

    removed checkbox input to replace with strike through later

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -3,7 +3,6 @@ import "./todolistitem.css";
 const TodoListItem = ({ todo }) => {
   return (
     <div className="listitem">
-      <input className="checked" type="checkbox" />
       <li>{todo}</li>
       <button>delete</button>
       <button>edit</button>
